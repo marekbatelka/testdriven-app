b4dcc7b6a49f535a07d351b1507f68a8f474a9e4
marekbatelka <2838809+marekbatelka@users.noreply.github.com>
2019-03-29 00:28:21 +0100

    part 1 chapter 6

--- project/__init__.py
+++ project/__init__.py
@@ -1,14 +1,15 @@
 # services/users/project/__init__.py
 
-
+import os  # new
 from flask import Flask, jsonify
-
 
 # instantiate the app
 app = Flask(__name__)
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)    
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')  # new
+app.config.from_object('project.config.DevelopmentConfig')
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
