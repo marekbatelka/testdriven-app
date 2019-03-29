fef0876977869eb228ef2dd5d291b8e835092d49
marekbatelka <2838809+marekbatelka@users.noreply.github.com>
2019-03-29 22:53:16 +0100

    Flask Blueprints Chapter 9

--- manage.py
+++ manage.py
@@ -1,20 +1,23 @@
 # services/users/manage.py
 
 
+import unittest
+
 from flask.cli import FlaskGroup
 
-from project import app
+from project import create_app, db   # new
+from project.api.models import User  # new
 
-import unittest
- 
-cli = FlaskGroup(app)
+app = create_app()  # new
+cli = FlaskGroup(create_app=create_app)  # new
 
-# new
+
 @cli.command('recreate_db')
 def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
+
 
 @cli.command()
 def test():
@@ -24,6 +27,7 @@ def test():
     if result.wasSuccessful():
         return 0
     return 1
+
 
 if __name__ == '__main__':
     cli()
