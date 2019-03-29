ff93d685368242b7322ff6b1e998133b1f2c8779
marekbatelka <2838809+marekbatelka@users.noreply.github.com>
2019-03-29 21:43:41 +0100

    added readme modified manage.py for next chapter added tests

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # testdriven-app
+
+
+What is this project?
+*********************
+This project is used as training repository.
+
+There is nice Flask and Microservices course at https://testdriven.io/courses/microservices-with-docker-flask-and-react/
+
+WARRANTY
+********
+NO WARRANTY.
+
+Credits
+*******
+All credits to testdriven.io
--- manage.py
+++ manage.py
@@ -5,7 +5,8 @@ from flask.cli import FlaskGroup
 
 from project import app
 
-
+import unittest
+ 
 cli = FlaskGroup(app)
 
 # new
@@ -14,6 +15,15 @@ def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
+
+@cli.command()
+def test():
+    """Runs the tests without code coverage"""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 if __name__ == '__main__':
     cli()
